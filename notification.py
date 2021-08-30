0669e01943283f1696a4fe17b725177c70aaa268
victorhuang <victorhuang@tencent.com>
2021-08-30 13:35:21 +0800

    change to pure python

--- notification.py
+++ notification.py
@@ -1,9 +1,15 @@
 import sys,os
+import requests
+from requests.structures import CaseInsensitiveDict
 
 def send_feishu(title=None,link=None):
-    cmd = """curl --header "Content-Type: application/json" --request POST --data '%s' https://www.feishu.cn/flow/api/trigger-webhook/a02be1e4229390baad907861213cd4c3"""
-    content = """{"events":[{"id":"%s","name":"%s"}]}"""%(link,title)
-    print(cmd % content)
-    os.system(cmd%content)
+    url = "https://www.feishu.cn/flow/api/trigger-webhook/a02be1e4229390baad907861213cd4c3"
 
-send_feishu("ttttt","https://www.qq.com")
+    headers = CaseInsensitiveDict()
+    headers["Content-Type"] = "application/json"
+
+    data = '{"events":[{"id":"%s","name":"%s"}]}'%(link,title)
+    resp = requests.post(url, headers=headers, data=data)
+    print(resp)
+
+send_feishu("ttt222tt","https://www.qq22.com")
